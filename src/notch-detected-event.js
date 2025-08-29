730bc5fb195a458f56a5f5f5e574e955fbfba208
joshmilroy <josh@orcascan.com>
2025-08-29 15:51:20 +0100

    add cordova plugin support for notch detection

--- src/notch-detected-event.js
+++ src/notch-detected-event.js
@@ -15,12 +15,10 @@
             checkForNotch(1000); // 1 sec
         });
     }
-    else {
-        // if not in Cordova, use the browser's 'load' event
-        window.addEventListener('load', function() {
-            checkForNotch(1000); // 1 sec
-        });
-    }
+            
+    window.addEventListener('load', function() {
+        checkForNotch(1000); // 1 sec
+    });
 
     window.addEventListener('orientationchange', function() {
         checkForNotch(0); // 0 sec
@@ -30,19 +28,42 @@
         checkForNotch(0); // 0 sec
     });
 
-    // main function to detect the presence of a notch
+    /**
+     * retrieves notch insets around mobile screen
+     * 
+     * @param {int} timeout - delay for load event listeners to get insets
+     * @returns {Object} - insets object containing top, bottom, left and right inset properties
+     */
     function checkForNotch(timeout) {
 
         // we need to wait before executing to allow layout to happen
         setTimeout(function() {
+
             // get the <html> element
             var root = document.documentElement;
 
             // add CSS variables so we can read them back
-            root.style.setProperty('--notch-top', 'env(safe-area-inset-top)');
-            root.style.setProperty('--notch-right', 'env(safe-area-inset-right)');
-            root.style.setProperty('--notch-bottom', 'env(safe-area-inset-bottom)');
-            root.style.setProperty('--notch-left', 'env(safe-area-inset-left)');
+            if (window.cordova && cordova.platformId === 'android' && cordova.plugins && cordova.plugins.safearea) {
+                // if using cordova and on android, then find safearea with plugin
+                cordova.plugins.safearea.getInsets().then(function (insets) {
+                    root.style.setProperty('--notch-top', (insets.top || 0) + "px");
+                    root.style.setProperty('--notch-right', (insets.right || 0) + "px");
+                    root.style.setProperty('--notch-bottom', (insets.bottom || 0) + "px");
+                    root.style.setProperty('--notch-left', (insets.left || 0) + "px");
+                }).catch(function () {
+                    // fallback on env variables
+                    root.style.setProperty('--notch-top', 'env(safe-area-inset-top)');
+                    root.style.setProperty('--notch-right', 'env(safe-area-inset-right)');
+                    root.style.setProperty('--notch-bottom', 'env(safe-area-inset-bottom)');
+                    root.style.setProperty('--notch-left', 'env(safe-area-inset-left)');
+                });
+            } else {
+                // ios devices use this
+                root.style.setProperty('--notch-top', 'env(safe-area-inset-top)');
+                root.style.setProperty('--notch-right', 'env(safe-area-inset-right)');
+                root.style.setProperty('--notch-bottom', 'env(safe-area-inset-bottom)');
+                root.style.setProperty('--notch-left', 'env(safe-area-inset-left)');
+            }
 
             // get runtime styles
             var computedStyle = window.getComputedStyle(root);
@@ -62,19 +83,12 @@
 
             // only if we have a notch
             if (hasNotch) {
-
                 // set <html data-notch="true"> to allow CSS to tweak the display
                 root.setAttribute('data-notch', 'true');
 
                 // fire global notch-detected event to let the UI know that happened
                 window.dispatchEvent(new CustomEvent('notch-detected', { bubbles: true, cancelable: true }));
             }
-
-            // remove CSS variables
-            root.style.removeProperty('--notch-top');
-            root.style.removeProperty('--notch-right');
-            root.style.removeProperty('--notch-bottom');
-            root.style.removeProperty('--notch-left');
         }, timeout);
     }
 
